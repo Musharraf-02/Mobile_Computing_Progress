ca2e035c7e07d6f2549aa7e67db0fed2b75990cb
Musharraf-02 <musharraf4102@gmail.com>
2021-03-27 14:19:41 +0500

    Seekbar

---
+++ Seekbar/app/src/main/java/com/example/seekbar/MainActivity.java
@@ -0,0 +1,51 @@
+package com.example.seekbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.SeekBar;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        SeekBar seekBar=findViewById(R.id.seekBar);
+        seekBar.setMin(1);
+        seekBar.setMax(40);
+        ArrayList<String> arrayList=new ArrayList<String>();
+        ListView listView=findViewById(R.id.listView);
+        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                //Log.d("Seekbar Value: ",String.valueOf(progress));
+                arrayList.clear();
+                for(int i=1;i<=10;i++)
+                {
+                    arrayList.add(progress+"     "+ "X" +"     "+i+"     "+"="+"     "+i*progress);
+                    ArrayAdapter<String> arrayAdapter=new ArrayAdapter<String>(getApplicationContext(), android.R.layout.simple_list_item_1,arrayList);
+                    listView.setAdapter(arrayAdapter);
+
+                }
+
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+    }
+}
